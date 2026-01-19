a6fbb5c91cba8a376b6760bc74aa1bb6f11cb635
Jonathan Sandoval <jonathansandovalferreira@gmail.com>
2026-01-19 08:54:45 -0400

    refactor: update dynamic export to force-static in multiple files and add robots.txt

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from "next/og"
 import { siteName } from "@/lib/seo"
 
-export const runtime = "edge"
+export const dynamic = "force-static"
 export const size = {
   width: 1200,
   height: 630,
